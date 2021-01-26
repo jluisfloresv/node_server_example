11e53b3b393bf71fe2fe0d70ee9ef251eaf907f4
= <=>
2021-01-26 15:10:16 -0600

    Adding listeners and use parameters by request

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log(request);
+
+  let url = request.url;
+  let method = request.method;
+
+  response.setHeader('Content-Type', 'text/html');
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+
+    //listeners
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const name = parseBody.split('=')[1];
+      response.write('<html>');
+      response.write('<h1>Welcome ' + name + '</h1>');
+      response.write('</html>');
+      response.end();
+    });
+    //end listeners
+  }
+
+  if (url === '/') {
+    response.write('<html>');
+    response.write('<h1>hello world, this is a node server basic</h1>');
+    response.write('<p>Human, What is your name?<p/>');
+    response.write('<form action="/message" method="POST">');
+    response.write('<input type="text" name="message" /><br>');
+    response.write('<button type="submit">OK</button><br>');
+    response.write('</form>');
+    response.write('</html>');
+    response.end();
+  }
+});
+
+//server config
+
+server.listen(3000);
